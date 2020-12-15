b7bf4fb55046751e9b362b010d1b419f9971f110
Max <maxblanck@outlook.fr>
2020-12-15 09:55:52 +0100

    php Partie 6  Exercice 3

--- index.php
+++ index.php
@@ -10,9 +10,16 @@
 <body>
     <h1>Exercice 3 Partie 6</h1>
     <?php
-    if (isset($_GET['startDate']) AND isset($_GET['endDate'])) : ?>
-        <p><?= 'From ' . $_GET['startDate'] . ' to ' . $_GET['endDate'] ?></p>
+    if (!isset($_GET['startDate'])):?>
+    <p>Début de période manquante</p>
+    <?php endif; ?>
+    <?php if(!isset($_GET['endDate'])): ?>
+    <p>Fin de la période manquante</p>
+    <?php endif; ?>
+    <?php if(isset($_GET['startDate']) && isset($_GET['endDate'])):?>
+        <p>Votre voyage du <?=  $_GET['startDate']?> au <?= $_GET['endDate'] ?></p>
     <?php endif ?>
+    <a href="index.php?startDate=2/05/2016&endDate=27/11/2016">Vérifier vos dates</a>
 </body>
 
 </html>
